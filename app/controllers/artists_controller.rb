8721ba47745deb02640e990421e3021a3ca01768
adam n <adam.nawroc@gmail.com>
2018-04-10 12:37:50 +0000

    progress

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,27 @@
 class ArtistsController < ApplicationController
+
+  def new
+    @artist = Artist.new
+
+  end
   def show
     @artist = Artist.find(params[:id])
 
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+    
+  end
+
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+
+  end
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
   end
 end
