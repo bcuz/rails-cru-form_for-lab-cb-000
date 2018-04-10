8dcd4db89fc28a8ba1a2a8022d375b65c54a81b8
adam n <adam.nawroc@gmail.com>
2018-04-10 13:21:04 +0000

    progress

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,50 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+
+  end
   def show
     @song = Song.find(params[:id])
     # binding.pry
 
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+
+  end
+
+  def update
+     # @post = Post.find(params[:id])
+     # @schoolclass = SchoolClass.find(params[:id])
+    #  @student = Student.find(params[:id])
+    #  @artist = Artist.find(params[:id])
+     @song = Song.find(params[:id])
+
+
+     # @post.update(title: params[:title], description: params[:description])
+     @song.update(song_params(:name, :artist_id, :genre_id))
+     #
+     # redirect_to post_path(@post)
+     redirect_to song_path(@song)
+
+   end
+
+  def new
+    @song = Song.new
+
+  end
+
+  def create
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
+
+  end
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
   end
 
 
